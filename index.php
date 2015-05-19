f813b5d620655e0d149154531b394dc0971f0421
zahidkhangurmani <zahid_gurmani@yahoo.com>
2015-05-19 21:32:28 +0500

    revert

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $userObj = "";
 if(isset($_SESSION['user'])){
 	$userObj = (object)	$_SESSION['user'];
 }
-sdasd
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
