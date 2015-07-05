0ccaf9e454064b64a604b793ccc8b8e32d06d63b
chumstech <m.umer1988@gmail.com>
2015-07-05 14:00:19 +0500

    User Layout Change

--- admin/set_owner.php
+++ admin/set_owner.php
@@ -242,168 +242,156 @@ while($data = mysql_fetch_array($Query))
 
 
 ?>
-
+<style>
+label {
+  float: left;
+  line-height: 25px;
+  margin-right: 5px;
+}
+select {
+  width: auto;
+  background-color: #fff;
+  border: 1px solid #ccc;
+}
+input, textarea, .uneditable-input {
+  width: auto;
+}
+</style>
 <form id="" name="form1" method="post" action="">
-<div id="form1">
-  <table width="100%" height="454" border="0" align="center" cellpadding="0" cellspacing="0">
-    <tr>
-      <td colspan="2"><?php echo @$msg;?></td>
-    </tr>
-    <tr>
-      <td colspan="2" class=""><h2>Client Configuration  </h2></td>
-    </tr>
-    <tr>
-      <td width="52%">First Name: </td>
-      <td width="48%"><?php echo @$First_Name; ?></td>
-    </tr>
-    <tr>
-      <td>Last Name : </td>
-      <td><?php echo @$Last_Name; ?></td>
-    </tr>
-    <tr>
-      <td>Address: </td>
-      <td><?php echo @$Address; ?></td>
-    </tr>
-    <tr>
-      <td>Country: </td>
-      <td><?php echo @$Country;?></td>
-    </tr>
-    <tr>
-      <td>Phone: </td>
-      <td><?php echo @$Phone_Number;?></td>
-    </tr>
-    <tr>
-      <td>Email:</td>
-      <td><?php echo @$Email;?></td>
-    </tr>
-    <tr>
-      <td height="41">Owner : </td>
-      <td> 
-	  <?php if ($userObj->user_type == 1 && $userObj->email) 
-		{
-	  ?>
-	  <select name="txt_owner" id="txt_owner" >  
-	  <?php
-	  	$QueryString = "select distinct * from users where user_type = 2";
-		$Query = mysql_query($QueryString);
-		while($data = mysql_fetch_array($Query)){
-	   ?>
-          <option value="<?php echo @$data['id'];?>" ><?php echo @$data['first_name']." ".@$data['last_name'];?></option>
-		<?php
-		}
-		?>
-      </select><?php }?>
-      <?php if ($userObj->user_type == 2 && $userObj->email) 
-		{
-	  ?>
-	  <input type="hidden" name="txt_owner" value="<?php echo @$owner; ?>"/>
- 		  <?php echo getUserName($owner);?>  
-      <?php }?>
-      </td>
-    </tr>
-    <tr>
-      <td>Show Posted Rates : </td>
-      <td><select name="show_posted" id="show_posted">  
-        <option <?php if($Posted_Rates == '1') { echo 'selected="selected"';}?> value="1">Yes</option>
-        <option <?php if($Posted_Rates == '0') { echo 'selected="selected"';}?> value="0">No</option>
-      </select>      </td>
-    </tr>
-    <tr>
-      <td>Show Discounted Rates : </td>
-      <td><select name="show_disc" id="show_disc">   
-        <option <?php if($Discounted_Rates == '1') { echo 'selected="selected"';}?> value="1">Yes</option>
-        <option <?php if($Discounted_Rates == '0') { echo 'selected="selected"';}?> value="0">No</option>
-      </select></td>
-    </tr>
-    <tr>
-      <td>Privilege Discount Enable: </td>
-      <td><select name="privilege_discount" id="privilege_discount">	   
-        <option <?php if($privilege_discount == '1') { echo 'selected="selected"';}?> value="1">Yes</option>
-        <option <?php if($privilege_discount == '0') { echo 'selected="selected"';}?> value="0">No</option>
-      </select></td>
-    </tr>
-    <?php $carriers = getCarriers();
-	$carriersOptions = '';
-		while($carrier = mysql_fetch_array($carriers))
-		{
-			//var_dump($carrier);
-			$DB_Carrier = checkCarrierAllowed($carrier['id'],$user_id);
-			if($DB_Carrier =='1'){ $checked = 'checked="checked"'; } else { $checked = '';}
-			$carriersOptions .= '<tr>';
-			  $carriersOptions .= '<td>'.$carrier['name'].' Enabled : </td>';
-			  $carriersOptions .= '<td><input type="checkbox" name="'.str_replace(' ', '_', strtolower($carrier['name'])).'" value="1" '.$checked.'/></td>';
-			$carriersOptions .= '</tr>';
-		}
-		echo $carriersOptions;
-	?>
-  </table>
-  </div>
-
-
-<!--<form id="form_discount" name="form_discount" method="post" action="admin/assign_discount.php">-->
-<div id="form_discount">
-  <table width="100%" border="0" align="center">
-    <tr>
-      <td colspan="2"><h2>Assign Discounts</h2></td>
-    </tr>
-	    <tr>
-      <td colspan="2"><div align="center"><?php //echo @$msg; ?></div></td>
-    </tr>
-	<?php $carriers = getCarriers();
-	$carriersOptions = '';
-		while($carrier = mysql_fetch_array($carriers))
-		{
-			//var_dump($carrier);
-			$DB_Carrier_Discount = checkCarrierDiscount($carrier['id'],$user_id);
-			$carriersDiscountOptions .= '<tr>';
-          		$carriersDiscountOptions .= '<td height="38">'.$carrier['name'].' %</td>';
-          		$carriersDiscountOptions .= '<td><input name="'.str_replace(' ', '_', strtolower($carrier['name'])).'_discount" type="text" id="'.str_replace(' ', '_', strtolower($carrier['name'])).'_discount" value="'.$DB_Carrier_Discount.'" /></td>';
-        	$carriersDiscountOptions .= '</tr>';
-		}
-		echo $carriersDiscountOptions;
-	?>
-  </table>
-  </div>
-<!--</form>-->
-
-
-<!--<form id="form_privilege_discount" name="form_privilege_discount" method="post" action="admin/assign_privilege_discount.php">-->
-<div id="form_privilege_discount">
-<!-- <input type="hidden"  name="atxt_Email" id="atxt_Email" value="<?php echo @$user_email; ?>"/> -->
-  <table width="100%" border="0" align="center">
-    <tr>
-      <td colspan="2"><h2>Assign Privilege Discounts</h2></td>
-    </tr>
-	    <tr>
-      <td colspan="2"><div align="center"><?php //echo @$msg; ?></div></td>
-    </tr>
-		<?php $carriers = getCarriers();
-            $carriersOptions = '';
-                while($carrier = mysql_fetch_array($carriers))
-                {
-                    //var_dump($carrier);
-                    $DB_Carrier_P_Discount = checkCarrierPriviligedDiscount($carrier['id'],$user_id);
-                    $carriersPDiscountOptions .= '<tr>';
-                        $carriersPDiscountOptions .= '<td height="38">'.$carrier['name'].' %</td>';
-                        $carriersPDiscountOptions .= '<td><input name="'.str_replace(' ', '_', strtolower($carrier['name'])).'_p_discount" type="text" id="'.str_replace(' ', '_', strtolower($carrier['name'])).'_discount" value="'.$DB_Carrier_P_Discount.'" /></td>';
-                    $carriersPDiscountOptions .= '</tr>';
-                }
-                echo $carriersPDiscountOptions;
-		?>
-  </table>
-  </div>
-  <div style="margin-top: 10px; margin-bottom: 10px; margin-right: 25px; float: left; width: 100% ! important;">
-  <?php if(isset($_GET['user_id'])){ ?>
-	  <input name="Update" class="btn btn-primary" type="submit" id="submit" value="Update" i />
-	  <?php }else{?>
-	  <input name="Submit" class="btn btn-primary" type="submit" id="Submit" value="Submit" /> 
-      <?php }?>
-      </div>
+	
+	<div class="container-fluid">
+    	<h2 style="margin-bottom: 5px;">User Personal Info</h2>
+        <div class="row-fluid">
+    		<div class="span6">
+        		<?php echo @$First_Name." ".@$Last_Name."<br/>"; ?>
+                <?php echo @$Address." ".getCountryByCode(@$data['country'])."<br/>"; ?>
+ 				<?php echo "Phone : ".@$Phone_Number."<br/>"; ?>
+    			<?php echo "Email : ".@$Email."<br/>"; ?>
+                <div class="form-group">
+            		<label style="">User Owner</label>
+                  <?php if ($userObj->user_type == 1 && $userObj->email) 
+				  {
+				  ?>
+				  <select class="form-control" name="txt_owner" id="txt_owner" >  
+				  <?php
+					$QueryString = "select distinct * from users where user_type = 2";
+					$Query = mysql_query($QueryString);
+					while($data = mysql_fetch_array($Query)){
+				   ?>
+					  <option value="<?php echo @$data['id'];?>" ><?php echo @$data['first_name']." ".@$data['last_name'];?></option>
+					<?php
+					}
+					?>
+				  </select><?php }?>
+				  <?php if ($userObj->user_type == 2 && $userObj->email) 
+					{
+				  ?>
+				  <input class="form-control" type="hidden" name="txt_owner" value="<?php echo @$owner; ?>"/>
+					  <?php echo getUserName($owner);?>  
+				  <?php }?>
+              </div>
+         	</div>
+        </div>
+		<div class="row-fluid">
+            <div class="span4">
+            <h2 style="margin-bottom: 5px;margin-top: 15px;width: 100%;">User Settings</h2>
+            	<div class="form-group">
+            		<label style="min-width:160px;">Show Posted Rates</label>
+            		<select class="form-control" name="show_posted" id="show_posted">  
+                        <option <?php if($Posted_Rates == '1') { echo 'selected="selected"';}?> value="1">Yes</option>
+                        <option <?php if($Posted_Rates == '0') { echo 'selected="selected"';}?> value="0">No</option>
+                     </select>
+            	</div>
+                <div class="form-group">
+            		<label style="min-width:160px;">Show Discounted Rates</label>
+            		<select class="form-control" name="show_disc" id="show_disc">   
+                        <option <?php if($Discounted_Rates == '1') { echo 'selected="selected"';}?> value="1">Yes</option>
+                        <option <?php if($Discounted_Rates == '0') { echo 'selected="selected"';}?> value="0">No</option>
+                      </select>
+            	</div>
+                <div class="form-group">
+            		<label style="min-width:160px;">Privilege Discount Enable</label>
+            		<select class="form-control" name="privilege_discount" id="privilege_discount">	   
+                        <option <?php if($privilege_discount == '1') { echo 'selected="selected"';}?> value="1">Yes</option>
+                        <option <?php if($privilege_discount == '0') { echo 'selected="selected"';}?> value="0">No</option>
+                      </select>
+            	</div>
+            	<?php $carriers = getCarriers();
+				$carriersOptions = '';
+					while($carrier = mysql_fetch_array($carriers))
+					{
+						//var_dump($carrier);
+						$DB_Carrier = checkCarrierAllowed($carrier['id'],$user_id);
+						if($DB_Carrier =='1'){ $checked = 'checked="checked"'; } else { $checked = '';}
+						$carriersOptions .= '<div class="form-group" style="float:left; width:100%;">';
+						  $carriersOptions .= '<label style="min-width:140px;">'.$carrier['name'].' Enabled</label>';
+						  $carriersOptions .= '<td><input type="checkbox" style="margin-top: 5px;" class="form-control" name="'.str_replace(' ', '_', strtolower($carrier['name'])).'" value="1" '.$checked.'/></td>';
+						$carriersOptions .= '</div>';
+					}
+					echo $carriersOptions;
+				?>
+         	</div>
+            <div class="span4">
+        		<h2 style="margin-bottom: 5px;margin-top: 15px;width: 100%;">Assign Discounts</h2>
+            	<?php $carriers = getCarriers();
+					$carriersOptions = '';
+						while($carrier = mysql_fetch_array($carriers))
+						{
+							$DB_Carrier_Discount = checkCarrierDiscount($carrier['id'],$user_id);
+							$carriersDiscountOptions .= '<div class="form-group">';
+								$carriersDiscountOptions .= '<label style="min-width:100px;">'.$carrier['name'].' %</label>';
+								$carriersDiscountOptions .= '<input style="max-width:50px" name="'.str_replace(' ', '_', strtolower($carrier['name'])).'_discount" type="text" id="'.str_replace(' ', '_', strtolower($carrier['name'])).'_discount" class="form-control" value="'.$DB_Carrier_Discount.'" />';
+							$carriersDiscountOptions .= '</div>';
+						}
+						echo $carriersDiscountOptions;
+					?>
+         	</div>
+            <div class="span4">
+        		<h2 style="margin-bottom: 5px;margin-top: 15px;width: 100%;">Assign Privilege Discounts</h2>
+                <?php $carriers = getCarriers();
+					$carriersOptions = '';
+						while($carrier = mysql_fetch_array($carriers))
+						{
+							//var_dump($carrier);
+							$DB_Carrier_P_Discount = checkCarrierPriviligedDiscount($carrier['id'],$user_id);
+							$carriersPDiscountOptions .= '<div class="form-group">';
+								$carriersPDiscountOptions .= '<label style="min-width:100px;">'.$carrier['name'].' %</label>';
+								$carriersPDiscountOptions .= '<td><input style="max-width:50px" name="'.str_replace(' ', '_', strtolower($carrier['name'])).'_p_discount" type="text" id="'.str_replace(' ', '_', strtolower($carrier['name'])).'_discount" class="form-control" value="'.$DB_Carrier_P_Discount.'" /></td>';
+							$carriersPDiscountOptions .= '</div>';
+						}
+						echo $carriersPDiscountOptions;
+				?>
+            </div>
+         </div>
+         <div class="row-fluid">
+    		<div class="span6">
+            	<div class="form-group" style="margin-top:10px;">
+				  <?php if(isset($_GET['user_id'])){ ?>
+                  <input name="Update" class="btn btn-primary" type="submit" id="submit" value="Update" i />
+                  <?php }else{?>
+                  <input name="Submit" class="btn btn-primary" type="submit" id="Submit" value="Submit" /> 
+                  <?php }?>
+                 </div>
+      		</div>
+      	</div>
+  	</div>
   </form>
 <!--</form>-->
 
 <style>
-
+@media (max-width: 859px) and (min-width: 540px)
+{
+#content_wrapper .span4 h2 {
+  background: grey none repeat scroll 0 0;
+  border-radius: 3px;
+  color: rgb(255, 255, 255);
+  font-size: 15px !important;
+  line-height: 20px;
+  margin: 0 0 20px;
+  padding: 10px 0 10px 3%;
+  width: 97%;
+}
+}
 h2 {
     color: #000;
     font-size: 20px;
@@ -411,33 +399,6 @@ h2 {
     line-height: 30px;
     margin: 0 0 30px;
     padding: 0;
-}
-
-
-#form1 {
-    border: 1px solid #ccc;
-    float: left;
-    margin-bottom: 25px;
-    padding: 5px 2% !important;
-    width: 27%;
-}
-#form_discount {
-    border: 1px solid #666;
-    float: left;
-    margin-left: 2%;
-    min-height: 455px;
-    padding: 5px 2% !important;
-    width: 27%;
-}
-
-
-#form_privilege_discount {
-    border: 1px solid #666;
-    float: left;
-    margin-left: 2%;
-    min-height: 455px;
-    padding: 5px 2% !important;
-    width: 27%;
 }
 </style>
 
